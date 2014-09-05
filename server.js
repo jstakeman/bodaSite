2c290a0a859923f5f4e3e538a0c0a0412feeddff
Jackson Stakeman <jstakeman@gmail.com>
2014-09-05 05:19:25 +0000

    fixed db connection

--- server.js
+++ server.js
@@ -17,10 +17,19 @@ var path = require('path');
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000 ;
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var DB_URL = process.env.OPENSHIFT_MONGODB_DB_URL || 'localhost' ;
-var DB_NAME = 'bodaSite' || 'test';
+
 
 //Connect to MongoDb
-mongoose.connect('mongodb://' + DB_URL + '/' + DB_NAME);
+if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+
+mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + 'bodaSite');
+}
+
+else {
+mongoose.connect('mongodb://localhost/test');
+}
+
+mongoose.connect(DB_URL + DB_NAME);
 var Guest = require('./guest');
 
 //Configure bodyParser
